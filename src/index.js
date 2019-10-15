fa956bf46f6134d00e59aa9010dd8349cfc70348
chels <chemccann@hotmail.com>
2019-10-15 18:27:54 -0400

    Added map svg on page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-import style from '/src/styles/index.scss';
+// import style from './styles/index.scss';
+
+let schoolData = []
+d3.json("src/data/schoolInfo.json", function(data){
+    data.map(el => {
+        //     el["hs-gpa-avg"] !== null ? console.log({"name": el["displayName"], "gpa": el["hs-gpa-avg"], "state": el["state"]}) : '' // 244 with GPA
+    //     // el["hs-gpa-avg"] === null ? console.log(el["displayName"], el["hs-gpa-avg"], el["state"]) : '' // shows colleges that show null gpa avg
+        schoolData.push({[el["displayName"]]: {"gpa": el["hs-gpa-avg"], "state": el["state"]}})
+        // d3.select("body").append("svg").append("")
+    })
+    console.log(schoolData)
+})
+
+// function myMap(){
+//     d3.select("svg")
+//         .append("image")
+//         .attr("xlink:href", "src/data/usa2.svg")
+// }
+
+function myMap(){
+    d3.select("body")
+        .append("img")
+        .attr("src", "src/data/usa2.svg")
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    // document.getElementById("canvas")
+    myMap();
+})
