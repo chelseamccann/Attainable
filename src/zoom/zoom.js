9186704f89fec276504a3a7820a83004eab49643
chels <chemccann@hotmail.com>
2019-10-17 16:09:59 -0400

    Adjusted zoom arguments

--- src/zoom/zoom.js
+++ src/zoom/zoom.js
@@ -2,9 +2,12 @@ export var zoom = function(){
 
 var svg = d3.select("body")
  .append("svg")
- .attr("width", "100%")
- .attr("height", "100%")
- .call(d3.zoom().on("zoom", function () {
+ .attr("width", "100%") // changes width of view box
+ .attr("height", "60%") // changes height of view box
+ .call(d3.zoom()
+//  .extent([[0,0], [100, 100]]) ??
+ .scaleExtent([1,3]) // limits how much you can zoom in
+ .on("zoom", function () {
     svg.attr("transform", d3.event.transform)
  }))
  .append("g")
