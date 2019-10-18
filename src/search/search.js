66d1cd2699b16bbcf5cab71b480d93b6313c0401
chels <chemccann@hotmail.com>
2019-10-17 21:52:16 -0400

    Updated search functionality to render list

--- src/search/search.js
+++ src/search/search.js
@@ -24,20 +24,34 @@ export var searchColleges = function(){
     .append("button")
     .attr("id", "searchTargetButton")
     .text("Search")
+
     
     // adding listener for current gpa input
     d3.select("#searchCurrentButton").on("click", function(){
+        d3.selectAll(".current").remove()
+        var list = d3.select("body").append("ul").attr("id", "school-list")
         var currentGPA = d3.select("#searchCurrent").node().value;
         var circles = d3.selectAll("circle")
+
         circles.style("display", function(d){
                 return d.gpa <= parseFloat(currentGPA) ? "block" : "none"
             }).attr("fill", function(d){
                 return  d.gpa <= parseFloat(currentGPA) ? "maroon" : ""
+            }).attr("",function(d){
+                if (d.gpa <= parseFloat(currentGPA)){ 
+                    debugger
+                    list.append("li")
+                    .text(d.schoolName)
+                    .attr("class", "current")
+                }
             })
+
     })
 
     // adding listener for target gpa input
     d3.select("#searchTargetButton").on("click", function(){
+        d3.selectAll(".target").remove()
+        var list = d3.select("body").append("ul").attr("id", "school-list")
         var currentGPA = d3.select("#searchCurrent").node().value;
         var targetGPA = d3.select("#searchTarget").node().value;
         var circles = d3.selectAll("circle")
@@ -49,6 +63,14 @@ export var searchColleges = function(){
                 } else if (parseFloat(targetGPA) > parseFloat(currentGPA) && d.gpa <= parseFloat(targetGPA)) {
                     return "yellow"
                 } 
+            })
+            .attr("",function(d){
+                if (parseFloat(targetGPA) > parseFloat(currentGPA) && d.gpa <= parseFloat(targetGPA)){ 
+                    debugger
+                    list.append("li")
+                    .text(d.schoolName)
+                    .attr("class", "target")
+                }
             })
     })
 
