00cd529da119678823dfe8e37295dd16e4a357e2
chels <chemccann@hotmail.com>
2019-10-17 22:25:19 -0400

    Setup css file for styling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
+import style from './styles/index.scss';
 import { mapData } from './map/map.js'
 import { searchColleges } from './search/search.js'
 import { zoom } from './zoom/zoom.js'
 
 document.addEventListener("DOMContentLoaded", () => {
-
-    d3.select("body")
-    .append("h1")
-    .text( "Attainable")
 
     searchColleges()
     zoom()
--- src/search/search.js
+++ src/search/search.js
@@ -3,33 +3,42 @@ export var searchColleges = function(){
 
 
     // adding text boxes 
-    d3.select("body")
+
+    d3.select("#layout")
+    .append("div")
+    .attr("id", "searches")
+
+    d3.select("#searches")
     .append("input")
     .attr("type", "text")
     .attr("id", "searchCurrent")
+    .attr("class", "current-box")
     .attr("placeholder", "Current GPA")
 
-    d3.select("body")
+    d3.select("#searches")
     .append("button")
     .attr("id", "searchCurrentButton")
+    .attr("class", "current-box")
     .text("Search")
 
-    d3.select("body")
+    d3.select("#searches")
     .append("input")
     .attr("type", "text")
     .attr("id", "searchTarget")
+    .attr("class", "target-box")
     .attr("placeholder", "Target GPA")
 
-    d3.select("body")
+    d3.select("#searches")
     .append("button")
     .attr("id", "searchTargetButton")
+    .attr("class", "target-box")
     .text("Search")
 
     
     // adding listener for current gpa input
     d3.select("#searchCurrentButton").on("click", function(){
         d3.selectAll(".current").remove()
-        var list = d3.select("body").append("ul").attr("id", "school-list")
+        var list = d3.select("#layout").append("ul").attr("id", "school-list")
         var currentGPA = d3.select("#searchCurrent").node().value;
         var circles = d3.selectAll("circle")
 
@@ -51,7 +60,7 @@ export var searchColleges = function(){
     // adding listener for target gpa input
     d3.select("#searchTargetButton").on("click", function(){
         d3.selectAll(".target").remove()
-        var list = d3.select("body").append("ul").attr("id", "school-list")
+        var list = d3.select("#layout").append("ul").attr("id", "school-list")
         var currentGPA = d3.select("#searchCurrent").node().value;
         var targetGPA = d3.select("#searchTarget").node().value;
         var circles = d3.selectAll("circle")
