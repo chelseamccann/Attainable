ad81585b4fb4020852d36aa0b038732a047d9a85
chels <chemccann@hotmail.com>
2019-10-20 16:16:54 -0400

    Heroku push

--- src/index.js
+++ src/index.js
@@ -9,4 +9,52 @@ document.addEventListener("DOMContentLoaded", () => {
     mapData()
     searchColleges()
     modal()
+
+// Refs
+const wrapCta  = document.querySelector('#wrap-cta'),
+btnCta   = document.querySelector('#cta'),
+  content  = document.querySelector('#layout'),
+btnClose = document.querySelector('#close');
+
+
+// Anime.js Commons Values for SVG Morph
+const common = {
+targets: '.polymorph',
+easing: 'easeOutQuad',
+duration: 600,
+loop: false
+};
+
+
+// Show content
+btnCta.addEventListener('click', () => {
+// Elements apparence
+wrapCta.classList.remove('active');
+content.classList.add('active');
+
+// Morph SVG
+anime({
+...common,
+points: [
+{ value: '215,110 0,110 186,86 215,0' }
+],
+});
+});
+
+
+// Hide content  
+btnClose.addEventListener('click', () => {
+// Elements apparence
+content.classList.remove('active');
+wrapCta.classList.add('active');
+
+// Morph SVG
+anime({
+...common,
+points: [
+{ value: '215,110 0,110 0,0 215,0' }
+]
+}); 
+});
+
 })
